2c4edfab79adec64ee41fbd4cc302bd02bc836fc
Srikanth Mohankumar <srikanth.mohan@tnqtech.com>
2025-06-06 16:59:45 +0530

    play with web2c

---
+++ wasm-builder/luatex.wasm/luatex/source/texk/web2c/luatexdir/luatexd.h
@@ -0,0 +1,5 @@
+#ifndef LUATEXD_H
+#define LUATEXD_H
+// Minimal stub header for WebAssembly build
+#include "luatex/source/texk/web2c/luatexdir/luatex.h"
+#endif
---
+++ wasm-builder/luatex.wasm/pdftexd.h
@@ -0,0 +1,5 @@
+#ifndef PDFTEXD_H
+#define PDFTEXD_H
+// Minimal stub header for WebAssembly build
+#include "luatex/source/texk/web2c/luatexdir/ptexlib.h"
+#endif
